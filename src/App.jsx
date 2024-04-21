7b259c3b2c05ea405bb9d667e3e0c0138ea0ca57
Hasan Basri ERTEKİN <hasanbasriertekin96@gmail.com>
2024-04-21 18:45:15 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-import { useState } from 'react'
-
-import './App.css'
-
-function App() {
-
+const App = () => {
   return (
     <>
-      
+      <div className="app">
+        <section className="side-bar">
+          <button>+ New Chat</button>
+          <ul className="history">
+            <li>BLUGH</li>
+          </ul>
+          <nav>
+            <p>Made by OnlyAI</p>
+          </nav>
+        </section>
+        <section className="main">
+          <h1>ChatGPT</h1>
+          <ul className="feed"></ul>
+          <div className="bottom-section">
+            <div className="input-container">
+              <input type="text" />
+              <div className="submit">➢</div>
+            </div>
+            <p className="info">
+              ChatGPT is a language model developed by OpenAI. It can assist you
+              in conversations, answer your questions, and provide suggestions
+              based on your interests.
+            </p>
+          </div>
+        </section>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
